ac6a1d4d94465d8c7dd258d7f667fccb1ba742f0
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-09-13 15:20:04 +0300

    Solved problem 33 on PE

--- ProjectEuler/prob24.py
+++ ProjectEuler/prob24.py
@@ -1,7 +1,7 @@
 # projecteuler.net/problem=24
 
 def main():
-    answer = LexPermutation('012')
+    answer = LexPermutation('0123')
     print(answer)
 
 def LexPermutation(n):
@@ -11,31 +11,38 @@ def LexPermutation(n):
     print(strlist)
     res = n
     counter = 0
-    while counter < 5:
+    while counter < 8:
         res = Permutate(strlist)
-        print(res)
+        #print(res)
         counter += 1
 
 def Permutate(n):
+    print(n)
     length = len(n)
     tail = []
 
     i = length-1
     while i > 0:
         if n[i] > n[i-1]:
-            if len(tail) > 0:
-                print("tail not empty")
-                tail = tail[::-1]
-                for j in range(i, len(tail)):
-                    n[j] = tail[j-i]
 
-            print('trace {0} - {1}'.format(n[i], n[i-1]))
             tmp = n[i]
             n[i] = n[i-1]
-            n[i-1] = tmp
+            n[i-1] = tmp            
 
-            
+            if len(tail) > 0:
+                #print("tail not empty")
+
+                tk = 0
+                k = i+1
+                while tk < len(tail):
+                    #print("{0} | {1} ({2})|({3})".format(n, tail, k, tk))
+                    n[k] = tail[tk]
+                    k += 1
+                    tk += 1
+                tail.clear()
             return n
+            #print('trace {0} - {1}'.format(n[i], n[i-1]))
+
         else:
             tail.append(n[i])
         i -= 1
