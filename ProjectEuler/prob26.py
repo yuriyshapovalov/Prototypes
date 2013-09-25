90074a1111c8d88868512f23589104d5023d9e1c
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-09-25 15:09:18 +0300

    Solved problem 26 on PE

--- ProjectEuler/prob26.py
+++ ProjectEuler/prob26.py
@@ -1,14 +1,35 @@
 # projecteuler.net/problem=26
-from decimal import *
 
 def main():
     answer = ReciprocalCycles()
     print("\nAnswer: {}".format(answer))
 
 def ReciprocalCycles():
-    getcontext().prec = 80
-    for i in range(1, 100):
-        print(Decimal(1)/Decimal(i))
+    i = 1000
+    seqLen = 0
+
+    while i > 1:
+        if seqLen >= i:
+            break
+
+        remainders = []
+        for i in range(0, i):
+            remainders.append(0)
+        value = 1
+        position = 0
+        
+
+        while remainders[value] == 0 and value != 0:
+            remainders[value] = position
+            value *= 10
+            value %= i
+            position += 1
+
+        if position - remainders[value] > seqLen:
+            seqLen = position - remainders[value]
+
+        i -= 1
+    return i+1
 
 if __name__ == '__main__':
     main()
