66cf3baf8fe6198691c3e3cb828fb5f72b640982
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-09-26 11:44:41 +0300

    Solved problem 44 on PE

--- ProjectEuler/prob44.py
+++ ProjectEuler/prob44.py
@@ -2,7 +2,7 @@
 
 def main():
     answer = PentagonNum()
-    print("Answer: {}".format(answer))
+    #print("Answer: {}".format(answer))
 
 def PentagonNum():
     pennum = []
@@ -10,9 +10,14 @@ def PentagonNum():
         n = int(((3 * (i * i)) - i) / 2)
         pennum.append(n)
     
-    for i in 
+    for i in pennum:
+        for j in pennum:
+            sm = i + j
+            dif = i - j
+            if sm in pennum and dif in pennum:
+                print("pair = {} {}: D {}".format(i, j, abs(i - j)))
 
-    return pennum
+    return 0
 
 if __name__ == '__main__':
     main()
