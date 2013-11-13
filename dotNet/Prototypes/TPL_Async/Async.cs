a57dd733dd7a0341463314c7f20ee894e0ada24b
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-11-13 23:56:21 +0200

    Added TPL Async and Multithreading project to .NET prototypes

---
+++ dotNet/Prototypes/TPL_Async/Async.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPL_Async
+{
+    class Async
+    {
+        static void Main(string[] args)
+        {
+            var web = new WebClient();
+            var web2 = new WebClient();
+
+            Task<string> googleTask = Task<string>.Factory.StartNew(() => { 
+                return web.DownloadString("http://google.com");
+            });
+
+            googleTask.ContinueWith(t => {
+
+                Console.WriteLine(googleTask.Result);
+            });
+
+            Task<string> yandexTask = web2.DownloadStringTaskAsync("http://yandex.ru");
+
+            Console.WriteLine(yandexTask.Result);
+
+            Thread.Sleep(10000);
+
+            Console.ReadKey();
+        }
+    }
+}
