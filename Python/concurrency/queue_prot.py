bd4567c7fab05eb42129070306cab646ecd2f98e
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-11-28 13:19:39 +0200

    Updated Python prototype queue

--- Python/concurrency/queue_prot.py
+++ Python/concurrency/queue_prot.py
@@ -1,7 +1,29 @@
 # queue - Synchronized queue class
+import queue
 
 def main():
     print("Queue object")
 
-is __name__ == '__main__':
+    q = queue.Queue()
+
+    y = q.empty()
+    print("queue.empty() = {}".format(y))
+
+    y = q.full()
+    print("queue.full() = {}".format(y))
+
+    q.put('some text')
+    print("queue.put('some text')")
+
+    q.put_nowait(311)
+    print("queue.put_nowait(311)")
+
+    y = q.get()
+    print("queue.get() = {}".format(y))
+
+    y = q.get_nowait()
+    print("queue.get_nowait() = {}".format(y))
+
+
+if __name__ == '__main__':
     main()
