3185c41f19f4ec151206e2fbe364b40e11a40ca8
yuriyshapovalov <shapovalov.yuri@gmail.com>
2023-09-27 23:39:04 +0100

    Typescript proto

---
+++ typescript/src/index.ts
@@ -0,0 +1,10 @@
+interface StringDictionary {
+    [key: string]: string;
+    propy: string;
+}
+
+const myDict: StringDictionary = {
+    'first': 'hello',
+    'second': 'world',
+    'propy': 'propy',
+};
